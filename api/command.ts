7409c1b81b268176c8c9cc0763544e7f7b05cff7
arre_ankit <arre.ankit@gmail.com>
2025-04-17 16:37:32 +0530

    🐛 FIX: command error

--- api/command.ts
+++ api/command.ts
@@ -1,15 +1,27 @@
 import { App } from '@slack/bolt';
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
 
+export async function POST(request: Request) {
 
-app.command('/build', async ({ command, ack, say }) => {
-  await ack();
-  await say(`Hello ${command.user_name}!`);
-});
+    const rawBody = await request.text();
+    const payload = JSON.parse(rawBody);
+    const requestType = payload.type as "url_verification" | "event_callback";
+
+    if (requestType === "url_verification") {
+        return new Response(payload.challenge, { status: 200 });
+    }
+
+    const app = new App({
+        token: process.env.SLACK_BOT_TOKEN,
+        signingSecret: process.env.SLACK_SIGNING_SECRET,
+    });
+
+    app.command('/build', async ({ command, ack, say }) => {
+        await ack();
+        await say(`Hello ${command.user_name}!`);
+    });
+
+}
 
 
 
