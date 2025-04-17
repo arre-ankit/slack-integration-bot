59ca518b6d263245b0355e8af383daa06b189186
arre_ankit <arre.ankit@gmail.com>
2025-04-17 16:39:39 +0530

    🐛 FIX: error

--- api/command.ts
+++ api/command.ts
@@ -14,6 +14,8 @@ export async function POST(request: Request) {
     const app = new App({
         token: process.env.SLACK_BOT_TOKEN,
         signingSecret: process.env.SLACK_SIGNING_SECRET,
+        socketMode: true,
+        appToken: process.env.SLACK_APP_TOKEN,
     });
 
     app.command('/build', async ({ command, ack, say }) => {
