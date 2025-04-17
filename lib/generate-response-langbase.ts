bd6868b53c38e47715d7182dfbca911b0c1d1371
arre_ankit <arre.ankit@gmail.com>
2025-04-17 17:29:40 +0530

    👌 IMPROVE: langbase

--- lib/generate-response-langbase.ts
+++ lib/generate-response-langbase.ts
@@ -7,8 +7,40 @@ export const generateResponseLangBase = async (
   updateStatus?: (status: string) => void,
 ) => {
 
+	await langbase.pipes.create({
+		name: 'command-agent-slack',
+		description: 'A pipe that can execute commands',
+		model: 'gpt-4o-mini',
+		upsert: true,
+		messages: [
+			{
+				role: 'system',
+				content: `You are a helpful AI assistant designed to assist users in building and running agents. 
+					Respond with one word very short
+					Your primary tasks include:
+
+						1. **Understanding User Intent**: Accurately interpret user requests related to building or running agents.
+						2. **Providing Clear Instructions**: Offer step-by-step guidance on how to build or run an agent, including any prerequisites or necessary configurations.
+						3. **Handling Missing Information**: If a user request lacks details, politely ask for the missing information to provide accurate assistance.
+						4. **Offering Examples**: Provide examples to illustrate how users can achieve their goals.
+
+						### Examples:
+
+						- **Example 1: Build an Agent that help users summarize thread**
+						- **User Input**: "I want to build an agent named 'WeatherBot'."
+						- **Assistant Response**: "/build"
+
+						- **Example 2: Running an Agent**
+						- **User Input**: "How do I run the 'StockAnalyzer' agent?"
+						- **Assistant Response**: "/run stock-analyser"
+
+						Remember, your goal is to be as helpful and informative as possible, ensuring users can successfully build and run their agents with your guidance.`,
+			}
+		]
+
+	});
     const commandResponse = await langbase.pipes.run({
-		name: 'command-agent',
+		name: 'command-agent-slack',
 		stream: false,
 		messages
 	});
